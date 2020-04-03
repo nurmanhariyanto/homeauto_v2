67bd41c4f4ffb82ce0363b0f9d761eb96e72de35
nurmanhariyanto <nurman.hariyanto13@gmail.com>
2020-04-03 17:58:15 +0700

    update worker and delete some file

--- Worker/WorkerMQTT/app.js
+++ Worker/WorkerMQTT/app.js
@@ -17,7 +17,7 @@ var splitdataOn
 var splitdataOff
 
 
-
+//connect mongodb 
 mongo.connect('mongodb://homeauto2:hom3auto2020!@dbmongo.server.pptik.id:27017/homeauto2', {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -28,65 +28,66 @@ db.on('error', error => console.error(error)
 )
 db.once('open', () => console.log('Berhasil Terhubung ke database'))
 
+//temporare data from db
 var dataTemp = ""
 
-  //connect rmq
-  var client = mqtt.connect('mqtt://167.205.7.226', {
-    username: '/homeauto:homeauto',
-    password: 'homeauto12345!',
-    port: 1883
+//connect rmq
+var client = mqtt.connect('mqtt://167.205.7.226', {
+  username: '/homeauto:homeauto',
+  password: 'homeauto12345!',
+  port: 1883
 })
 
-  //check db
-  Configuration.findOne({ mac: Mac }).sort({ $natural: -1 }).then(data => {
-    dataTemp = data
-    valueData = dataTemp.value
-    splitData = valueData.split("")
+//check db to the last data from database
+Configuration.findOne({ mac: Mac }).sort({ $natural: -1 }).then(data => {
+  dataTemp = data
+  valueData = dataTemp.value
+  splitData = valueData.split("")
+  console.log(splitData)
+
+  // check incoming message to parsing each element of  array from schedule
+
+
+  //if schedule to turn on device 
+  if (jadwal == "1") {
+    splitdataOn = command.split("")
+    numberArrayOn = (splitdataOn.indexOf('1'))
+    console.log(numberArrayOn)
+
+    for (i = 0; i < splitData; i++) {
+      splitData[i] = splitData[i - 1]
+
+    }
+    splitData[numberArrayOn] = "1"
+
+    splitData = splitData
+    console.log("on")
+
+  }
+  //if schedule to turn off device 
+  if (jadwal == "0") {
+    splitdataOff = command.split("")
+    numberArrayOff = (splitdataOff.indexOf('0'))
+    console.log(numberArrayOff)
+    for (i = 0; i < splitData; i++) {
+      splitData[i] = splitData[i - 1]
+
+    }
+    splitData[numberArrayOff] = "0"
+
     console.log(splitData)
+    splitData = splitData
+    console.log("off")
+  }
 
-    if (jadwal == "1") {
-      splitdataOn = command.split("")
-      numberArrayOn = (splitdataOn.indexOf('1'))
-      console.log(numberArrayOn)
 
-      for (i = 0 ; i < splitData; i++){
-        splitData[i]= splitData[i-1]
-        
-      }
-      splitData[numberArrayOn] = "1"
+  var messageCommand = splitData.join("")
+  console.log(messageCommand)
+  //publish message
+  client.publish(Mac, messageCommand)
 
-     splitData = splitData
-     console.log("on")
+})
 
-    }
-
-    if (jadwal == "0") {
-      splitdataOff = command.split("")
-      numberArrayOff = (splitdataOff.indexOf('0'))
-      console.log(numberArrayOff)
-      for (i = 0 ; i < splitData; i++){
-        splitData[i]= splitData[i-1]
-        
-      }
-      splitData[numberArrayOff] = "0"
-
-      console.log(splitData)
-      splitData = splitData
-      console.log("off")
-    }
-
- var messageCommand = splitData.join("")
- console.log(messageCommand)
-
-    // a = dataParse.indexOf('0')
-    // for(var i =0 ; i <= dataParse.length;i++){
-    //   a = dataParse.indexOf('0')
-    // }
-    //publish
-    client.publish(Mac,messageCommand)
-
-  })
-
-  setTimeout((function() {
-    process.exit()
-  }),5000);
+setTimeout((function () {
+  process.exit()
+}), 10000);
